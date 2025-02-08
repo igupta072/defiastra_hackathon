3f432cec41d4a68b7035dbda219425506263546f
Indrajeet Gupta <indrajeet.gupta@coindcx.com>
2025-02-08 10:10:08 +0530

    gitignore file added

--- lib/module/dashboard/home_controller.dart
+++ lib/module/dashboard/home_controller.dart
@@ -2,14 +2,5 @@ import 'package:get/get.dart';
 
 class HomeController extends GetxController {
 
-  List<String> get items => [
-    '1',
-    '2',
-    '3',
-    '4',
-    '5',
-    '6',
-    '7',
-    '8',
-  ];
+
 }
